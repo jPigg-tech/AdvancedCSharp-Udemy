190fa292abb4d7f95fb8f33d0952e64532aeb3dc
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-10 16:41:05 -0500

    Implemented a new class inside of the Generics class called DiscountCalculator That is a generic type and shows the Where T : Product constraints. How a TProduct has access to all of the products fields and properties

--- AdvancedCSharp/Generics.cs
+++ AdvancedCSharp/Generics.cs
@@ -26,7 +26,31 @@ namespace AdvancedCSharp
         public T Max(T a, T b)
         {
             return a.CompareTo(b) > 0 ? a : b;
+
+            // if a compared to b is greater than 0
+            // return a otherwise return b
+        }       
+    }
+
+    public class Product
+    {
+        public string  Title { get; set; }
+        public float Price { get; set; }
+    }
+    public class Book : Product
+    {
+        public string Isbn { get; set; }
+    }
+    public class DiscountCalculator<TProduct> where TProduct : Product
+    {
+        public float CalculateDiscount(TProduct product)
+        {
+            return product.Price;
         }
 
+        // We don't care about the actual discount
+        // This just shows with that constraint if you pass
+        // in a TProduct you'll have access to all of its 
+        // properties and methods using dot notation
     }
 }
