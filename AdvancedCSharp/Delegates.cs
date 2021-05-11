2ea989351a027bb816edd281080971a2b5ac10ef
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-11 17:49:19 -0500

    Implemented a Photo class with a couple methods. and a PhotosFilters class that will be used when we implement the delecates

--- AdvancedCSharp/Delegates.cs
+++ AdvancedCSharp/Delegates.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace AdvancedCSharp
 {
+    // Delegates are for designing extensible and 
+    // flexable apllications (they are similar to Interfaces)
+    // A delegate is a pointer to a function(a reference to a function)
+    // An object that knows how to call a method(or multiple)
+    // Use a delegate over a interface when you dont need to access
+    // other properties or methods on the object implementing the method
     class Delegates
     {
+
     }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+        public void Save()
+        {
+
+        }
+    }
+    public class PhotoFilters
+    {
+        // For simplicity I have just used Console.WriteLine
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightess");
+        }
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+
+    
 }
