9892228f15ca8cdd2e1adafafead9f3698b19e01
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-12 14:00:51 -0500

    Implemented a class for LambdaExpressions with a couple notes for myself

---
+++ AdvancedCSharp/LambdaExpressions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp
+{
+    // An anonymous method 
+    // no access modifier
+    // no name 
+    // no retrun statement
+    // We use them for convenience
+    // Write less code and achieve the same thing 
+    // and code is more readable
+    class LambdaExpressions
+    {
+    }
+}
